8af0620adb58bfb7d9a202682fcb0fd8c1fb969c
Michelle123211 <M.stolova@seznam.cz>
2022-02-25 15:20:55 +0100

    Added Dungeon scene + objects to transition

--- PeacefulAdventure/Assets/Scripts/DoorBehaviour.cs
+++ PeacefulAdventure/Assets/Scripts/DoorBehaviour.cs
@@ -6,13 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class DoorBehaviour : Interactable
 {
+    [SerializeField] string nextScene;
     protected override void OnInteraction(InputAction.CallbackContext context) {
+        SceneManager.LoadScene(this.nextScene);
         string sceneName = SceneManager.GetActiveScene().name;
-        if (sceneName == "MainMap") {// change scene from MainMap to HouseIndoor
-            SceneManager.LoadScene("HouseIndoor");
-            Debug.Log("To HouseIndoor");
-        } else if (sceneName == "HouseIndoor") {// change scene from HouseIndoor to MainMap
-            SceneManager.LoadScene("MainMap");
-        }
     }
 }

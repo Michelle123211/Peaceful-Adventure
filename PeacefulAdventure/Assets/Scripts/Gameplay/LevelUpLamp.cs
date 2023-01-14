0ead507c31760ca36d6570567003d1bc53a8ba11
Michelle123211 <M.stolova@seznam.cz>
2023-01-14 23:48:40 +0100

    Lamp particles when active

--- PeacefulAdventure/Assets/Scripts/Gameplay/LevelUpLamp.cs
+++ PeacefulAdventure/Assets/Scripts/Gameplay/LevelUpLamp.cs
@@ -8,6 +8,7 @@ public class LevelUpLamp : Interactable
 {
 
     public Light2D lampLight;
+    public ParticleSystem lampParticles;
 
     protected override void OnInteraction(InputAction.CallbackContext context) {
         // level the player up if they have enough XP
@@ -36,8 +37,10 @@ public class LevelUpLamp : Interactable
     private void UpdateState() {
         if (PlayerState.Instance.levelSystem.CanLevelUp()) {
             lampLight.color = Color.green;
+            lampParticles.Play();
         } else {
             lampLight.color = Color.red;
+            lampParticles.Stop();
         }
     }
 }

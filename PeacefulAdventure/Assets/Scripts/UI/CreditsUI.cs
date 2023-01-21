245b8136ab17ad770a86717640c909be1e19684a
Michelle123211 <M.stolova@seznam.cz>
2023-01-21 14:36:29 +0100

    Credits + settings fix

--- PeacefulAdventure/Assets/Scripts/UI/CreditsUI.cs
+++ PeacefulAdventure/Assets/Scripts/UI/CreditsUI.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CreditsUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private bool initialized = false;
+
+    public void Open() {
+        PlayerBehaviour.playerInputActions.Player.Disable();
+        PlayerBehaviour.playerInputActions.UI.Enable();
+        AudioManager.Instance.PlaySoundEffect(SoundType.UIOpen);
+        gameObject.TweenAwareEnable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Close() {
+        PlayerBehaviour.playerInputActions.UI.Disable();
+        PlayerBehaviour.playerInputActions.Player.Enable();
+        AudioManager.Instance.PlaySoundEffect(SoundType.UIClose);
+        gameObject.TweenAwareDisable();
+    }
+
+    public void SetInitialized() {
+        initialized = true;
+    }
+
+    private void OnEnable() {
+        initialized = false;
+        // register input callbacks
+        PlayerBehaviour.playerInputActions.UI.Action3_L.performed += ReturnToMenu;
+    }
+
+    private void OnDisable() {
+        // unregister input callbacks
+        PlayerBehaviour.playerInputActions.UI.Action3_L.performed -= ReturnToMenu;
+    }
+
+    private void ReturnToMenu(InputAction.CallbackContext context) {
+        if (initialized) {
+            AudioManager.Instance.PlaySoundEffect(SoundType.UIPress);
+            Close();
+        }
     }
 }
